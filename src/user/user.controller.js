90bdf261cb2accf48d71c907f39286f61db13f6c
grill112233 <grill_0822553047@hotmail.com>
2020-11-25 16:17:30 +0700

    pipeline successV2

---
+++ src/user/user.controller.js
@@ -0,0 +1,43 @@
+const User = require('./user.model');
+
+module.exports.check = async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+  if (!user) {
+    throw Error('User not found');
+  }
+
+  next();
+};
+
+module.exports.create = async (req, res) => {
+  const user = new User(req.body);
+  await user.save();
+  
+  res.json(user);
+};
+
+module.exports.remove = async (req, res) => {
+  await User.findByIdAndRemove(req.params.id);
+
+  res.json(req.params.id);
+};
+
+module.exports.list = async (req, res) => {
+  const user = await User.find({});
+  
+  res.json(user);
+};
+
+module.exports.update = async (req, res) => {
+  const user = await User.findOneAndUpdate({ _id: req.params.id }, req.body, {
+    new: true
+  }).exec();
+
+  
+};
+
+module.exports.view = async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  res.json(user);
+};
